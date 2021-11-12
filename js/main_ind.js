0cf70e1ce98a6a1bcefcd1252898c22f4f717e6a
Trevertor23 <bogdan.v.228@gmail.com>
2021-11-12 21:27:40 +0200

    Added done tasks from pr 4.2

--- js/main_ind.js
+++ js/main_ind.js
@@ -9,12 +9,28 @@ let users = [
     {login:"trevertor",password:"321654321"},
     {login:"guest",password:"12345"}
 ];
-
+let openStatus = 0;
 
 
 function toogleModalAuth(){
    modalAuth.classList.toggle('is-open');
+   openStatus++;
+   document.getElementById('login').style.borderColor = '';
+   document.getElementById('login').value = '';
+   document.getElementById('password').style.borderColor = '';
+   document.getElementById('password').value = '';
+   if(openStatus == 2){
+        document.body.style.overflow = 'visible';
+        openStatus = 0;
+   }
+   else{
+       document.body.style.overflow = 'hidden';
+   }
 }
+modalAuth.addEventListener('click', function(event){
+    if(event.target.classList.contains('is-open'))
+        toogleModalAuth();
+})
 
 buttonAuth.addEventListener('click', function(){
     
@@ -51,7 +67,15 @@ if(authData !== null)
                     document.getElementById('login-text').innerText = login;
                     break;
                 }
-                else if (i == users.length-1){authStatus = false; localStorage.setItem('authStatus',false); alert('Error! Please, check your login and password, try again.');break;}
+                else if (i == users.length-1){
+                    authStatus = false; localStorage.setItem('authStatus',false);
+                    alert('Error! Please, check your login and password, try again.');
+                    if(login=='')
+                        document.getElementById('login').style.borderColor = '#ff0000';
+                    else if(password=='')
+                        document.getElementById('password').style.borderColor = '#ff0000';
+                    break;
+                }
             }
     })
 // else if (authStatus == true) { 
